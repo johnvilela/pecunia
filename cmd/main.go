f25523d0cebae5bf8b863fc0bc6b42b0df0625ae
John Vilela <johnvvilela@gmail.com>
2026-09-17 09:43:57 -0300

    chore: bump version to 0.9.0

--- cmd/main.go
+++ cmd/main.go
@@ -13,7 +13,7 @@ import (
 
 // version is the single source of truth for releases: bumping it on master
 // makes CI tag and publish v<version>.
-var version = "0.8.0"
+var version = "0.9.0"
 
 const banner = `
  ________  _______    ________  ___  ___  ________   ___  ________
